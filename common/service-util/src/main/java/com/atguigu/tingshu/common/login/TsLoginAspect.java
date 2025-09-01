dd1f9d008c4d30cc23fc792e6444a9a45be2d1b7
陆小凤一笑 <714448168@qq.com>
2025-09-01 19:13:30 +0800

    更新专辑和声音

--- common/service-util/src/main/java/com/atguigu/tingshu/common/login/TsLoginAspect.java
+++ common/service-util/src/main/java/com/atguigu/tingshu/common/login/TsLoginAspect.java
@@ -1,4 +1,20 @@
 package com.atguigu.tingshu.common.login;
 
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
 public class TsLoginAspect {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+
+
+
 }
